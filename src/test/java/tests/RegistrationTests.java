5bd45c37db29c69031e68515c9d363252e2f9568
TayaHatum <21taya21@gmail.com>
2022-03-11 12:01:48 +0200

    commit testbase

---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase{
+
+
+
+    @Test
+    public void registrationSuccess(){
+// open regform
+        //fill email
+        //fill password
+        //click registration
+
+        //Assert
+    }
+}
