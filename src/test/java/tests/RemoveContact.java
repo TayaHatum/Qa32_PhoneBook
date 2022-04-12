a36635ba66e10269b87ee2730116d503aa6508a0
TayaHatum <21taya21@gmail.com>
2022-04-12 21:51:06 +0300

    logger

--- src/test/java/tests/RemoveContact.java
+++ src/test/java/tests/RemoveContact.java
@@ -1,2 +1,17 @@
-package tests;public class RemoveContact {
+package tests;
+
+import org.testng.annotations.Test;
+
+public class RemoveContact extends TestBase{
+
+
+    @Test
+    public void removeOneContact(){
+
+    }
+
+    @Test
+    public void removeAllContact(){
+
+    }
 }
